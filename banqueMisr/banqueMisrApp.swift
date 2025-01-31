257d4f856463656bd8649749c15409a72e8608d5
Hady Helal <>
2025-01-31 11:16:57 +0200

    initial commit

---
+++ banqueMisr/banqueMisrApp.swift
@@ -0,0 +1,17 @@
+//
+//  banqueMisrApp.swift
+//  banqueMisr
+//
+//  Created by Hady Helal on 31/01/2025.
+//
+
+import SwiftUI
+
+@main
+struct banqueMisrApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ banqueMisrTests/banqueMisrTests.swift
@@ -0,0 +1,17 @@
+//
+//  banqueMisrTests.swift
+//  banqueMisrTests
+//
+//  Created by Hady Helal on 31/01/2025.
+//
+
+import Testing
+@testable import banqueMisr
+
+struct banqueMisrTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
